4fe33d5ceee6a020a155802054f415db3ef6693f
francesco <zf8190@gmail.com>
2025-06-19 16:44:44 +0200

    primo commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# New Project
+
+Python backend for Serie A top 10 transfer news site.
---
+++ app/models/__init__.py
@@ -0,0 +1,5 @@
+from .base import Base  # unica fonte di verità per Base
+from .team import Team
+from .feed import Feed
+from .article import Article
+from .feed_per_team import feed_per_teams
